6b7296acbac7ac82ccb5da4e6a84afe3f0e3e59c
Alvin Gao <gaovinal@gmail.com>
2020-10-11 02:37:39 -0400

    Added bot functionality

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import logging as log
 import discord
+import os
+import clap_converter
+
+
+class ClapBot(discord.Client):
+    async def on_ready(self):
+        log.info('ClapBot started with username {0.name} and id {0.id}'.format(self.user))
+
+    async def on_message(self, message):
+        if len(message.mentions) > 0 and message.mentions[0].id == self.user.id:
+            await message.channel.send(clap_converter.clapify(message.content))
+
+
+client = ClapBot()
+client.run(os.getenv('DISCORD_KEY'))
